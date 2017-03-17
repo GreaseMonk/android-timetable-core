98822c2747acd710b6fd78d0eab914854bbacf0c
W Geertsma <anacondah@gmail.com>
2017-03-17 11:30:34 +0100

    - Updated FastAdapter library - Updated support libraries

--- src/main/java/com/greasemonk/timetable/TimeTable.java
+++ src/main/java/com/greasemonk/timetable/TimeTable.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
@@ -14,7 +13,7 @@ import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
-import com.mikepenz.fastadapter.adapters.FastItemAdapter;
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
 
 import java.util.*;
 
